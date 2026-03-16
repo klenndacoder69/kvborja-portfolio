cbe12e4d49c9c6474934e7358d876db0ad652fed
klenndacoder69 <kvborja@up.edu.ph>
2026-03-17 04:34:02 +0800

    added github actions

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+		// Replace 'kvborja-portfolio' with your actual GitHub repo name.
+		// Remove this line entirely if your repo is <username>.github.io
+		paths: { base: dev ? '' : '/kvborja-portfolio' }
 	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
